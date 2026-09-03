233097900a7b9285c2b06d9842b27a646be54928
masharipov2105 <masharipov2105@gmail.com>
2026-09-03 12:25:52 -0400

    The utility layer was developed, and all tests were written and successfully completed

--- src/main/java/com/masharipov2105/systems/utils/InputValidator.java
+++ src/main/java/com/masharipov2105/systems/utils/InputValidator.java
@@ -10,14 +10,13 @@ public final class InputValidator{
 		throw new UnsupportedOperationException("Utility class");
 	}
 
-
+	//parsing number method
 	public static double parseNumber(String number) throws InvalidNumberException, NullPointerException{
 
 		if (number == null || number.trim().isEmpty()){
 
 			throw new NullPointerException("The number cannot be null or empty");
 		}
-
 		try{
 
 			return Double.parseDouble(number.trim());
@@ -27,21 +26,18 @@ public final class InputValidator{
 		}
 	}
 
-
+	//parsing command method
 	public static char parseCommand(String command) throws NullPointerException, InvalidCommandException{
 
 		final char[] commands = {'+', '-', '*', '/'}; 
-
 		if (command == null || command.trim().isEmpty()){
 
 			throw new NullPointerException("The command cannot be null or empty");
 		}
-
 		if (command.trim().length() > 1){
 
 			throw new InvalidCommandException(command.trim());
 		}
-
 		for (char i: commands){
 
 			if (command.charAt(0) == i){
@@ -49,7 +45,40 @@ public final class InputValidator{
 				return command.charAt(0);
 			}
 		}
-
 		throw new InvalidCommandException(command.trim());
+	}
+
+	//isYes method
+
+	public static Boolean isYes(String command) throws NullPointerException {
+
+		if (command == null || command.trim().isEmpty()){
+
+			throw new NullPointerException("The command cannot be null or empty");
+		}
+
+		return (command.equals("yes") || command.equals("y"));
+	}
+
+	//isNo method
+	public static Boolean isNo(String command) throws NullPointerException {
+
+		if (command == null || command.trim().isEmpty()){
+
+			throw new NullPointerException("The command cannot be null or empty");
+		}
+
+		return (command.equals("no") || command.equals("n"));
+	}
+
+	//isExit command method
+	public static Boolean isExit(String command) throws NullPointerException {
+
+		if (command == null || command.trim().isEmpty()){
+
+			throw new NullPointerException("The command cannot be null or empty");
+		}
+
+		return (command.equals("exit") || command.equals("quit"));
 	}
 }
--- src/test/java/com/masharipov2105/systems/utils/InputValidatorTest.java
+++ src/test/java/com/masharipov2105/systems/utils/InputValidatorTest.java
@@ -72,4 +72,96 @@ public class InputValidatorTest{
 		Exception exp2 = assertThrows(InvalidCommandException.class, ()->{InputValidator.parseCommand("2");});
 		assertEquals("2 command not found !", exp2.getMessage());
 	}
+
+	//============================== IsYes =========================================
+
+	@Test
+	void testIsYesSuccess() throws NullPointerException{
+
+		assertEquals(true, InputValidator.isYes("yes"));
+		assertEquals(true, InputValidator.isYes("y"));
+	}
+
+	@Test
+	void testIsYesNullCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isYes(null);});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsYesEmptyCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isYes("");});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsYesNotYesCommand() throws NullPointerException{
+
+		assertEquals(false, InputValidator.isYes("iueroiwer703"));
+	}
+
+
+
+
+	//============================== IsNo =========================================
+
+	@Test
+	void testIsNoSuccess() throws NullPointerException{
+
+		assertEquals(true, InputValidator.isNo("no"));
+		assertEquals(true, InputValidator.isNo("n"));
+	}
+
+	@Test
+	void testIsNoNullCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isNo(null);});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsNoEmptyCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isNo("");});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsNoNotNoCommand() throws NullPointerException{
+
+		assertEquals(false, InputValidator.isNo("iueroiwer703"));
+	}
+
+
+
+	//============================== IsExit =========================================
+
+	@Test
+	void testIsExitSuccess() throws NullPointerException{
+
+		assertEquals(true, InputValidator.isExit("exit"));
+		assertEquals(true, InputValidator.isExit("quit"));
+	}
+
+	@Test
+	void testIsExitNullCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isExit(null);});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsExitEmptyCommand() throws NullPointerException{
+
+		Exception exp = assertThrows(NullPointerException.class, ()->{InputValidator.isExit("");});
+		assertEquals("The command cannot be null or empty", exp.getMessage());
+	}
+
+	@Test
+	void testIsExitNotExitCommand() throws NullPointerException{
+
+		assertEquals(false, InputValidator.isExit("iueroiwer703"));
+	}
 }
