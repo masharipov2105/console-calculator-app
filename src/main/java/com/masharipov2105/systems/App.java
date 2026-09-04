c0ec3335701978de950f90276633a15c576e50b5
masharipov2105 <masharipov2105@gmail.com>
2026-09-04 07:01:14 -0400

    The App class was built, the application flow was established, and manual testing was performed

---
+++ src/main/java/com/masharipov2105/systems/App.java
@@ -0,0 +1,114 @@
+package com.masharipov2105.systems;
+
+//imports
+import com.masharipov2105.systems.service.CalculatorService;
+import com.masharipov2105.systems.exceptions.*;
+import com.masharipov2105.systems.models.CalculatorModel;
+import com.masharipov2105.systems.utils.InputValidator;
+import java.util.Scanner;
+
+public class App{
+
+	//fields
+	private CalculatorService cs;
+	private Scanner scanner;
+	private CalculatorModel cm;
+	private boolean isRun = true;
+
+	private String banner = 
+		"\n==================================\n" +
+		"==    Console Calculator App    ==\n" +
+		"==================================\n";
+
+	//constructor
+	public App(CalculatorService calculatorService){
+
+		this.cs = calculatorService;
+		this.cm = new CalculatorModel("", "", ' ');
+		this.scanner = new Scanner(System.in);
+	}
+
+	// methods
+	public void run() throws CalculatorException{
+
+		System.out.println(this.banner);
+		String num1 = "";
+		String num2 = "";
+		String command = "";
+
+		while(this.isRun){
+
+			try{
+
+				System.out.print("enter number 1: ");
+				num1 = this.scanner.nextLine();
+				if (InputValidator.isExit(num1)){
+					isRun = false;
+					System.out.println("\nGoodbye");
+					break;
+				}
+				System.out.print("enter number 2: ");
+				num2 = this.scanner.nextLine();
+				if (InputValidator.isExit(num2)){
+					isRun = false;
+					System.out.println("\nGoodbye");
+					break;
+				}
+				System.out.print("enter command(+, -, *, /): ");
+				command = this.scanner.nextLine();
+				if (InputValidator.isExit(command)){
+					isRun = false;
+					System.out.println("\nGoodbye");
+					break;
+				}
+				InputValidator.parseCommand(command);
+
+				this.cm.setFirstNumber(num1);
+				this.cm.setSecondNumber(num2);
+				this.cm.setCommand(command.charAt(0));
+
+				System.out.println(this.cm + " = " + this.cs.calculate(this.cm));
+				toAsk();
+
+			} catch(InvalidNumberException e){
+
+				System.out.println(e.getMessage());
+				toAsk();
+			} catch(InvalidCommandException e){
+
+				System.out.println(e.getMessage());
+				toAsk();
+			} catch(DivisionByZeroException e){
+
+				System.out.println(e.getMessage());
+				toAsk();
+			} catch(CalculatorException e){
+
+				System.out.println(e.getMessage());
+				toAsk();
+			} catch (Exception e){
+
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	private void toAsk() throws NullPointerException{
+
+		System.out.print("\nDo you want to continue? (yes/no): ");
+		String answer = this.scanner.nextLine();
+
+		try{
+
+			if (InputValidator.isYes(answer)){
+				this.isRun = true;
+			} else if (InputValidator.isNo(answer)){
+				this.isRun = false;
+				System.out.println("\nGoodbye");
+			}
+		} catch(NullPointerException e){
+
+			System.out.println(e.getMessage());
+		}
+	}
+}
--- src/main/java/com/masharipov2105/systems/Main.java
+++ src/main/java/com/masharipov2105/systems/Main.java
@@ -1,8 +1,12 @@
 package com.masharipov2105.systems;
+import com.masharipov2105.systems.App;
+import com.masharipov2105.systems.exceptions.CalculatorException;
+import com.masharipov2105.systems.service.CalculatorServiceImpl;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CalculatorException{
         
-        System.out.println("Hello world!");
+        App app = new App(new CalculatorServiceImpl());
+        app.run();
     }
 }
